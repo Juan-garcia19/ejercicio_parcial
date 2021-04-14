6a1da3a0d9d286b89bfce309a2dee7de969662b4
Juan-garcia19 <jesteban.garcia1@udea.edu.co>
2021-04-13 20:42:01 -0500

    segunda fase del ejercicio

--- main.cpp
+++ main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include <math.h>
+#define G 9.81
+#define pi 3.141617
 
 using namespace std;
 
+struct Do{
+    float d = 800;
+    float Ho = 100;
+    float Xo = 0;
+    float Yo = Ho;
+    float d0=0.05*d;
+};
+struct Dd{
+    float d = 800;
+    float Hd = 200;
+    float Xd = d;
+    float Yd = Hd;
+    float d0=0.025*d;
+};
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    Do disparoO;
+    Dd disparoD;
+    int Voo;
+    cout << "Ingrese la Vo para la simulacion:";
+    cin>>Voo;
     return 0;
 }
