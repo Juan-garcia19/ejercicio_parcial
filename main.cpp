c3eb797bce4c3587d4eff345ae578284fa6733e5
Juan-garcia19 <jesteban.garcia1@udea.edu.co>
2021-04-13 21:58:32 -0500

    tercera fase

--- main.cpp
+++ main.cpp
@@ -20,6 +20,27 @@ struct Dd{
     float d0=0.025*d;
 };
 
+void DisparoOfensivo( Do disparoO,Dd disparoD,int Voo){
+    float x,y;
+    float Vxo,Vyo;
+    float Vini =0;
+    int t= 0;
+    int angle =0;
+    for ( Vini = Voo;;Vini+=5){
+        for(angle =0;angle < 90;angle++){
+            Vxo = Vini*cos(angle*pi/180);
+            Vyo = Vini*sin(angle*pi/180);
+            for(t = 0;;t++){
+                x= Vxo*t;
+                y= disparoO.Yo + Vyo*t - (0.5*G*t*t);
+                if (sqrt(pow((disparoD.Xd-x),2)+pow((disparoD.Yd-y),2)) < disparoO.d0){
+
+            }
+            }
+        }
+    }
+}
+
 int main()
 {
     Do disparoO;
